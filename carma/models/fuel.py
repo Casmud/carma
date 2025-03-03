fb05e1efcceaf4f9341a8bbac64d03e5ac8ab93c
Gijs Mos <gijs.mos+gh@matanga.nl>
2025-03-03 12:17:45 +0100

    some remarks on models/fuel

--- carma/models/fuel.py
+++ carma/models/fuel.py
@@ -55,9 +55,15 @@ class Fuel(CarmaBase, table=True):
 
         """
         fuel_records_df = pd.DataFrame([vars(fuel_record) for fuel_record in fuel_records])
+        # TODO (Gijs comment): Fuel is a Pydantic subclass, so you should use fuel_record.model_dump() instead of vars(fuel_record)
+        # This also will make sure that only the "defined payload attributes" are included in the DataFrame
 
         # Extract company names and add to temp_df
         fuel_records_df["company_name"] = fuel_records_df["company"].apply(lambda x: x.name)
+        # TODO (Gijs comment): This may get costly. Firstly the lambda function is executed for 
+        # every row in the DataFrame, jumping from Python to C and back. Secondly, this may result
+        # in a lot of queries to the database. It is better to use a join in the original query to
+        # get the company name in the same query.
 
         fuel_records_df = fuel_records_df[["date", "company_name", "mileage", "liters", "price"]]
         fuel_records_df["date"] = pd.to_datetime(fuel_records_df["date"])
@@ -67,6 +73,8 @@ class Fuel(CarmaBase, table=True):
         # Calculate km_driven
         fuel_records_df["km_driven"] = fuel_records_df["mileage"].diff()
         fuel_records_df["km_driven"] = fuel_records_df["km_driven"].fillna(0)  # First entry has no previous mileage
+        # TODO (Gijs comment): Here you replace all NaN values with 0, not only the first one.
+        # This may not be a problem, but they should not be there. Also what about negative and zero values?
 
         # Calculate price_per_liter
         fuel_records_df["price_per_liter"] = fuel_records_df["price"] / fuel_records_df["liters"]
@@ -108,3 +116,26 @@ class Fuel(CarmaBase, table=True):
 
         fuel_records_df = fuel_records_df.sort_index()
         return fuel_records_df
+
+# TODO (Gijs comment): Alternative get all fuel records you need with all computed stuff in one query. It may need a bit of further tweaking
+# to get it to work, but it should be possible. This way you can avoid the pandas processing step.
+def get_augmented_fuel_records(from_date: datetime.date | None = None, to_date: datetime.date | None = None):
+    with rx.session() as session:
+        query = select(
+            Fuel, 
+            (Fuel.mileage - rx.func.lag(Fuel.mileage).over(order_by=Fuel.date)).label("delta_mileage"),
+            (Fuel.price / Fuel.liters).label("price_per_liter"),
+            Company.name.label("company_name"),
+            (rx.func.coalesce((Fuel.mileage - rx.func.lag(Fuel.mileage).over(order_by=Fuel.date)) / Fuel.liters, 0)).label("consumption")
+        ).join(Company, Fuel.company_id == Company.id)
+        
+        if from_date is not None:
+            query = query.where(Fuel.date >= from_date)
+        
+        if to_date is not None:
+            query = query.where(Fuel.date <= to_date)
+        
+        results = session.exec(query)
+        fuel_records = results.all()
+        
+        return fuel_records
